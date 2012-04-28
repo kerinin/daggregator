1ef1e21f4804d80373b39aa5289c2c445d5ab509
Ryan Michael <kerinin@gmail.com>
2012-04-28 10:55:52 -0500

    initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Daggregator::Application.config.secret_token = '016d72e5f5c7d6bf98fcd72cbd5347cc1c6f74e43876fe4a6faff0e234b959906f831fdbb3b2a7e8717155b0d27e1bf1da5608b62ac30611eb013fc0e1ba5af9'
